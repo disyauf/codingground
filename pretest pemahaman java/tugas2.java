57088dc00ab966d76ca6f9c40310288a7aa1b305
disyauf <disyauf281@gmail.com>
2017-03-06 08:55:36 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas2.java
@@ -0,0 +1,10 @@
+public class tugas2{
+
+     public static void main(String []args){
+        String[] charArray ={ "andis", "syaefudin" }; 
+        
+        for (int i = 0; i < charArray.length; i++) {
+         System.out.println(charArray[i] + " ");
+        }
+     }
+}
