5ad98ee8724fb2edecc5e241bc8ab4467842c3a1
disyauf <disyauf281@gmail.com>
2017-03-06 08:54:05 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+
+     public static void main(String []args){
+        String nama = "andis";
+        String kota = "Jepara";
+        System.out.println(nama + " " + kota);
+     }
+}
